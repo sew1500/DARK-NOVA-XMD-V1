54b405b34d62a118802acad38c4028156163f370
PRIME-ALPHA-X-DULA-MD <dulinanethmira626@gmail.com>
2025-08-15 18:48:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,5 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "uS5EGQ6R#ej_vIiKLq4hyBsH2ELVMo2Elq3X2xZmknkHqzB2eAl8",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:pzpFHALBVOXYCqDKIKorAcqvlJBabaOH@crossover.proxy.rlwy.net:26835",
   OWNER_NUM: process.env.OWNER_NUM || "94752978237 / 94770349867",
 };
