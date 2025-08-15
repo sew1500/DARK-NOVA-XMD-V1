5bc808fa16c2a2a68a6b496f73b9951c77bd1641
PRIME-ALPHA-X-DULA-MD <dulinanethmira626@gmail.com>
2025-08-15 19:06:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,8 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "uS5EGQ6R#ej_vIiKLq4hyBsH2ELVMo2Elq3X2xZmknkHqzB2eAl8",
   OWNER_NUM: process.env.OWNER_NUM || "94752978237 / 94770349867",
   PREFIX: process.env.PREFIX ||"."
+  ALIVE_IMG:process.env.ALIVE_IMG || "https://github.com/dula9x/DARK-NOVA-XMD-V1-WEB-PAIR/blob/main/images/WhatsApp%20Image%202025-08-15%20at%2017.22.03_c520eb7b.jpg?raw=true",
+  ALIVE_MSG: process.env.ALIVE_MSG || "Iam Alive Now!! ᴅᴀʀᴋ ɴᴏᴠᴀ xᴍᴅ 🤭💗 ආහ් පැටියෝ කොහොමද ?🌝!\n\n🥶𝐌𝐚𝐝𝐞 𝐛𝐲 alpha x team🥶",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "thrue",
+  MODE: process.env.MODE || "public",
 };
