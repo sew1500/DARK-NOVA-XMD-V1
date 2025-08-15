ea48f264e7bd3ad06e48010ddde717072a232c2b
PRIME-ALPHA-X-DULA-MD <dulinanethmira626@gmail.com>
2025-08-15 18:57:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "uS5EGQ6R#ej_vIiKLq4hyBsH2ELVMo2Elq3X2xZmknkHqzB2eAl8",
   OWNER_NUM: process.env.OWNER_NUM || "94752978237 / 94770349867",
+  PREFIX: process.env.PREFIX ||"."
 };
