74953dea8c8dee84228f891811dd1f054863e89a
𝙿𝚁𝙸𝙼𝙴 𝙰𝙻𝙿𝙷𝙰-𝙼𝙳 𝚅 01.0 <primealpha727@gmail.com>
2025-08-15 21:08:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "uS5EGQ6R#ej_vIiKLq4hyBsH2ELVMo2Elq3X2xZmknkHqzB2eAl8",
+  SESSION_ID: process.env.SESSION_ID || "7CJwBDyQ#y0avic_VElgTpQI-tWcdlp6ZYOu6PhJjKMWeSijK2-I",
   OWNER_NUM: process.env.OWNER_NUM || "94752978237 / 94770349867",
   PREFIX: process.env.PREFIX ||"."
   ALIVE_IMG:process.env.ALIVE_IMG || "https://github.com/dula9x/DARK-NOVA-XMD-V1-WEB-PAIR/blob/main/images/WhatsApp%20Image%202025-08-15%20at%2017.22.03_c520eb7b.jpg?raw=true",
