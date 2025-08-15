94fc3078990edadb4fec7fdd1cff3e7ff0d0df93
𝙿𝚁𝙸𝙼𝙴 𝙰𝙻𝙿𝙷𝙰-𝙼𝙳 𝚅 01.0 <primealpha727@gmail.com>
2025-08-15 21:17:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "uS5EGQ6R#ej_vIiKLq4hyBsH2ELVMo2Elq3X2xZmknkHqzB2eAl8",
+  SESSION_ID: process.env.SESSION_ID || "7CJwBDyQ#y0avic_VElgTpQI-tWcdlp6ZYOu6PhJjKMWeSijK2-I",
   MONGODB: process.env.MONGODB || "mongodb://mongo:pzpFHALBVOXYCqDKIKorAcqvlJBabaOH@crossover.proxy.rlwy.net:26835",
   OWNER_NUM: process.env.OWNER_NUM || "94752978237 / 94770349867",
 };
